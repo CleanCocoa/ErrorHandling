d02480d302f45b68c99675b7edbd3cf7a0ce2ded
Christian Tietze <christian.tietze@gmail.com>
2015-10-21 13:58:26 +0200

    Initial Commit

---
+++ ErrorHandling/ErrorHandling.h
@@ -0,0 +1,19 @@
+//
+//  ErrorHandling.h
+//  ErrorHandling
+//
+//  Created by Christian Tietze on 21/10/15.
+//  Copyright © 2015 Christian Tietze. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ErrorHandling.
+FOUNDATION_EXPORT double ErrorHandlingVersionNumber;
+
+//! Project version string for ErrorHandling.
+FOUNDATION_EXPORT const unsigned char ErrorHandlingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ErrorHandling/PublicHeader.h>
+
+

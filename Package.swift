cd9815828fe14ca21a1708dfb95ee3d3f4ecd02d
Christian Tietze <me@christiantietze.de>
2022-12-22 13:47:49 +0100

    make compatible with SPM

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ErrorReporter",
+    platforms: [
+        .macOS(.v10_13),
+    ],
+    products: [
+        .library(
+            name: "ErrorReporter",
+            targets: ["ErrorReporter"]),
+    ],
+    targets: [
+        .target(
+            name: "ErrorReporter",
+            dependencies: [])
+    ]
+)
